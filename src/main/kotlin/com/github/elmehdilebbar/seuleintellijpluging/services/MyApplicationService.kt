0ca6bc3476a7b2c66fcc331031879bb1dd0563a1
GitHub Action <action@github.com>
2022-07-27 11:20:28 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/elmehdilebbar/seuleintellijpluging/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.elmehdilebbar.seuleintellijpluging.services
+
+import com.github.elmehdilebbar.seuleintellijpluging.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/elmehdilebbar/seuleintellijpluging/services/MyProjectService.kt
+++ src/main/kotlin/com/github/elmehdilebbar/seuleintellijpluging/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.elmehdilebbar.seuleintellijpluging.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.elmehdilebbar.seuleintellijpluging.MyBundle
 
 class MyProjectService(project: Project) {
 
